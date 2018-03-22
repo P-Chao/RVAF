9b1ffeb3283ccb9edec931fa24c02db2c5d815bb
PC <1377318286@qq.com>
2018-03-22 10:36:59 +0800

    add note for BinoTrackLayer.cpp

--- SVAF/layer/BinoTrackLayer.cpp
+++ SVAF/layer/BinoTrackLayer.cpp
@@ -14,46 +14,52 @@ namespace svaf{
 
 bool BinoTrackLayer::reinit_;
 
-// 
+// 构造函数
 BinoTrackLayer::BinoTrackLayer(LayerParameter& layer) : Layer(layer), adaboost(NULL), scalefactor_(-1)
 {
-	init_type_ = layer.bittrack_param().init_type();
+	// 参数读入
+	init_type_ = layer.bittrack_param().init_type(); // 初始化目标位置方式
 	track_type_ = layer.bittrack_param().track_type();
-	trackcount_ = layer.bittrack_param().track_count();
+	trackcount_ = layer.bittrack_param().track_count(); // 连续跟踪帧数量
 	
+	// haar特征数量
 	min_rectnum_ = layer.bittrack_param().haarmin_rectnum();
 	max_rectnum_ = layer.bittrack_param().haarmax_rectnum();
 
 	init_negnum_ = layer.bittrack_param().init_negnum();
-	negnum_ = layer.bittrack_param().negnum();
+	negnum_ = layer.bittrack_param().negnum();	// 负样本个数
 	posmax_ = layer.bittrack_param().posmax();
 	numsel_ = layer.bittrack_param().numsel();
 	numfeat_ = layer.bittrack_param().numfeat();
 	srchwinsz_ = layer.bittrack_param().srchwinsz();
 	negsample_strat_ = layer.bittrack_param().negsample_strat();
 
+	// 学习率
 	lrate_ = layer.bittrack_param().lrate();
 	posrad_ = layer.bittrack_param().posrad();
 	init_posrad_ = layer.bittrack_param().init_posrad();
 
+	// 跟踪控制参数
 	pc::binotrack_param.tss = layer.bittrack_param().tss();
 	pc::binotrack_param.sync = layer.bittrack_param().sync();
 	pc::binotrack_param.pool = layer.bittrack_param().pool();
 	pc::binotrack_param.mixfeat = layer.bittrack_param().mixfeat();
 	pc::binotrack_param.showprob = layer.bittrack_param().showprob();
 
+	// 跟踪框的尺度参数
 	if (layer.bittrack_param().has_scalefactor()){
-		scalefactor_ = layer.bittrack_param().scalefactor();
+		scalefactor_ = layer.bittrack_param().scalefactor(); // 为加速跟踪算法，跟踪框处理图像常常经过缩放
 	} else{
 		trsize_.width = layer.bittrack_param().tr_width();
 		trsize_.height = layer.bittrack_param().tr_height();
 	}
-
+	
+	// 初始化目标位置的方式
 	switch (init_type_)
 	{
-	case svaf::MilTrackParameter_InitType_MOUSE:
+	case svaf::MilTrackParameter_InitType_MOUSE: // 使用鼠标勾选目标位置
 		break;
-	case svaf::MilTrackParameter_InitType_SELECT:
+	case svaf::MilTrackParameter_InitType_SELECT: // 在脚本中设定目标位置
 		if (layer.bittrack_param().init_rect_size() == 0){
 			LOG(FATAL) << "Init Rect Has Not Set!";
 		}
@@ -66,9 +72,9 @@ BinoTrackLayer::BinoTrackLayer(LayerParameter& layer) : Layer(layer), adaboost(N
 			init_rects_.push_back(rect);
 		}
 		break;
-	case svaf::MilTrackParameter_InitType_AUTORECT:
+	case svaf::MilTrackParameter_InitType_AUTORECT: // 自动选择中心进行跟踪
 		break;
-	case svaf::MilTrackParameter_InitType_ADABOOST:
+	case svaf::MilTrackParameter_InitType_ADABOOST: // 根据目标检测算法初始化目标位置
 		if (layer.has_adaboost_param()){
 			adaboost = new AdaboostLayer(layer);
 		} else{
@@ -80,19 +86,20 @@ BinoTrackLayer::BinoTrackLayer(LayerParameter& layer) : Layer(layer), adaboost(N
 		break;
 	}
 
+	// 跟踪类型
 	switch (track_type_)
 	{
 	default:
-	case svaf::MilTrackParameter_TrackType_MIL:
-	case svaf::MilTrackParameter_TrackType_ADA:
+	case svaf::MilTrackParameter_TrackType_MIL: // MIL算法进行跟踪
+	case svaf::MilTrackParameter_TrackType_ADA: // Adaboost检测
 		LOG(ERROR) << "track type error, default use MIL_GRAY";
-	case svaf::MilTrackParameter_TrackType_MIL_GRAY:
+	case svaf::MilTrackParameter_TrackType_MIL_GRAY: // MIL灰度图像跟踪
 		binotrack_firstframe = pc::miltrack_firstframe_sync;
 		binotrack_frame = pc::miltrack_frame_sync;
 		break;
-	case svaf::MilTrackParameter_TrackType_MIL_RGB:
-	case svaf::MilTrackParameter_TrackType_MIL_LUV:
-	case svaf::MilTrackParameter_TrackType_MIL_HSV:
+	case svaf::MilTrackParameter_TrackType_MIL_RGB: // MIL使用RGB图像跟踪
+	case svaf::MilTrackParameter_TrackType_MIL_LUV: // MIL使用LUV色彩空间进行跟踪
+	case svaf::MilTrackParameter_TrackType_MIL_HSV: // MIL使用HSV空间进行跟踪
 		binotrack_firstframe = pc::miltrack_firstframe_sync_mc;
 		binotrack_frame = pc::miltrack_frame_sync_mc;
 		break;
@@ -100,14 +107,17 @@ BinoTrackLayer::BinoTrackLayer(LayerParameter& layer) : Layer(layer), adaboost(N
 
 }
 
+// 析构函数，释放资源
 BinoTrackLayer::~BinoTrackLayer(){
 	if (adaboost){
 		delete adaboost;
 	}
 }
 
+// 运行跟踪算法
 bool BinoTrackLayer::Run(std::vector<Block>& images, vector<Block>& disp, LayerParameter& layer, void* param){
 	if ((*id) == 0 || reinit_ || (*id) % trackcount_ == 0){
+		// 在特定帧初始化目标位置
 		trackers_.resize(images.size());
 		insize_.resize(images.size());
 		winsize_.resize(images.size());
@@ -120,6 +130,7 @@ bool BinoTrackLayer::Run(std::vector<Block>& images, vector<Block>& disp, LayerP
 		reinit_ = !InitFirstFrame(images, layer);
 		LOG(INFO) << trackers_.size() <<" Trackers Has Been Created.";
 	} else {
+		// 非特定帧运行跟踪算法
 		for (int i = 0; i < images.size(); ++i){
 			CHECK_EQ(insize_[i], images[i].image.size()) << "Image Size Changed!";
 		}
@@ -132,6 +143,7 @@ bool BinoTrackLayer::Run(std::vector<Block>& images, vector<Block>& disp, LayerP
 		return false;
 	}
 
+	// 恢复尺度（为加速跟踪算法，跟踪框处理图像常常经过缩放）
 	RecoverScale(images, disp);
 
 	for (int i = 0; i < images.size(); ++i){
@@ -145,9 +157,11 @@ bool BinoTrackLayer::Run(std::vector<Block>& images, vector<Block>& disp, LayerP
 	return true;
 }
 
+// 视频跟踪
 bool BinoTrackLayer::TrackFrame(vector<Block>& images){
 	for (int i = 0; i < images.size(); ++i){
 		cv::resize(images[i].image, trackers_[i].img, trackers_[i].trsize);
+		// 对于彩色图像选取处理的颜色空间
 		if (trackers_[i].img.channels() == 3){
 			switch (track_type_)
 			{
@@ -173,6 +187,7 @@ bool BinoTrackLayer::TrackFrame(vector<Block>& images){
 		}
 	}
 	__t.StartWatchTimer();
+	// 输入参数运行跟踪
 	binotrack_frame(trackers_[0].img, trackers_[0].rect, trackers_[0].trparam, trackers_[0].ftrparam,
 		trackers_[1].img, trackers_[1].rect, trackers_[1].trparam, trackers_[1].ftrparam);
 	__t.ReadWatchTimer("Bino Track Time");
@@ -182,11 +197,13 @@ bool BinoTrackLayer::TrackFrame(vector<Block>& images){
 	return true;
 }
 
+// 目标位置的初始化
 bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer){
 	vector<Rect> ada_rect;
+	// Adaboost目标检测方式初始化
 	if (init_type_ == MilTrackParameter_InitType_ADABOOST){
 		bool retval = adaboost->RunForOneRect(images, layer, ada_rect);
-		if (!retval){
+		if (!retval){ // 检测失败，需要重新运行初始化
 			reinit_ = true;
 			LOG(ERROR) << "Adaboost Init Rect Failed.";
 			return false;
@@ -196,6 +213,7 @@ bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer
 	for (int i = 0; i < images.size(); ++i){
 		switch (init_type_){
 		case svaf::MilTrackParameter_InitType_MOUSE:
+			// 使用鼠标勾选目标位置
 			pc::Rect r;
 			pc::g_org = images[i].image;
 			pc::g_org.copyTo(pc::g_img);
@@ -211,6 +229,7 @@ bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer
 			destroyWindow("Select Track Target");
 			break;
 		case svaf::MilTrackParameter_InitType_SELECT:
+			// 使用脚本设置的像素坐标
 			if (i >= init_rects_.size()){
 				init_rect_ = init_rects_[0];
 			} else{
@@ -218,12 +237,14 @@ bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer
 			}
 			break;
 		case svaf::MilTrackParameter_InitType_AUTORECT:
+			// 图像中心坐标为目标位置
 			init_rect_.width = images[i].image.cols / 12;
 			init_rect_.height = images[i].image.rows / 8;
 			init_rect_.x = images[i].roi.width / 2 - init_rect_.width / 2;
 			init_rect_.y = images[i].roi.height / 2 - init_rect_.width / 2;
 			break;
 		case svaf::MilTrackParameter_InitType_ADABOOST:
+			// 使用Adaboost检测得到的位置坐标
 			init_rect_ = ada_rect[i];
 			break;
 		default:
@@ -237,6 +258,7 @@ bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer
 		ComputeScale(i);
 	}
 
+	// 按照尺度或大小缩放图像并转换色彩空间
 	for (int i = 0; i < images.size(); ++i){
 		cv::resize(images[i].image, trackers_[i].img, trackers_[i].trsize);
 		if (trackers_[i].img.channels() == 3){
@@ -261,6 +283,7 @@ bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer
 			}
 		}
 	}
+	// 运行跟踪
 	__t.StartWatchTimer();
 	binotrack_firstframe(trackers_[0].img, trackers_[0].rect, trackers_[0].trparam, trackers_[0].ftrparam,
 		trackers_[1].img, trackers_[1].rect, trackers_[1].trparam, trackers_[1].ftrparam);
@@ -273,6 +296,7 @@ bool BinoTrackLayer::InitFirstFrame(vector<Block>& images, LayerParameter& layer
 	return true;
 }
 
+// 初始化跟踪参数
 void BinoTrackLayer::InitSetParam(BoostTrack& tracker){
 	
 	tracker.ftrparam.minRectNum = min_rectnum_;
@@ -307,6 +331,7 @@ void BinoTrackLayer::InitSetParam(BoostTrack& tracker){
 
 }
 
+// 计算目标尺度和尺度因子
 void BinoTrackLayer::ComputeScale(int i){
 	if (scalefactor_ > 0){
 		//for (int i = 0; i < trackers_.size(); ++i){
@@ -334,6 +359,7 @@ void BinoTrackLayer::ComputeScale(int i){
 	}
 }
 
+// 恢复目标尺度
 void BinoTrackLayer::RecoverScale(vector<Block>& images, vector<Block>& disp){
 
 	if (scalefactor_ > 0){
